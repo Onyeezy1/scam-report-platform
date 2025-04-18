cc1f2dcf7bdfe8e3770a912aa06cbfda508ab2c5
Onyeezy1 <145129671+Onyeezy1@users.noreply.github.com>
2025-04-18 01:05:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -185,7 +185,11 @@ def approve_report(report_id):
     
 
 
+from os import environ
+
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    port = int(environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
